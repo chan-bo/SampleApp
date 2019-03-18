12edee4f37eb7c5db02461ca1b8d490ef4c45845
Chanbo <chanbo97@gmail.com>
2019-03-18 15:12:02 +0700

    merge from koin

--- fastlane/send_to_slack.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -F token=xoxp-474526796610-473921783169-555578726836-5aa8b4ad476a130b89634de997c2fdf9 -F channels=CDYAFET1A -F file=@../app/build/outputs/apk/debug/app-debug.apk -F initial_comment=Newest\ Debug\ Apk https://slack.com/api/files.upload
